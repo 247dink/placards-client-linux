6ceb6ff772705ebfe9a8fd0a16ae5ac7bb9d9c17
Ben Timby <btimby@gmail.com>
2025-06-05 14:50:14 -0400

    Remove user (use current).

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
 #!/bin/sh -x
 
-# TODO: not sure if this is needed.
-CHROME_USER="${CHROME_USER:-orangepi}"
-CHROME_USER_HOME="$(eval echo ~${CHROME_USER})"
 EXEC_NAMES="chrome google-chrome chromium chromium-browser"
-CONFIG_PATH="${CONFIG_PATH:-${CHROME_USER_HOME}/.placards.ini}"
+CONFIG_PATH="${CONFIG_PATH:-~/.placards.ini}"
 CHROME_PROFILE_DIR="${CHROME_PROFILE_DIR:-/var/tmp/placards}"
 
 mkdir -p "${CHROME_PROFILE_DIR}"
-chown -R ${CHROME_USER} "${CHROME_PROFILE_DIR}"
+chown -R ${USER} "${CHROME_PROFILE_DIR}"
 
 # Try to find chrome executable...
 for EXEC_NAME in "${EXEC_NAMES}"; do
@@ -19,12 +16,12 @@ for EXEC_NAME in "${EXEC_NAMES}"; do
 done
 
 if [ -z "${CHROME_BIN_PATH}" ]; then
-    apt install -y chromium-browser
+    sudo apt install -y chromium-browser
     CHROME_BIN_PATH=$(which chromium-browser)
 fi
 
 if ! which pip; then
-    apt install -y python3-pip
+    sudo apt install -y python3-pip
 fi
 
 echo EOF > "${CONFIG_PATH}" << EOF
@@ -33,13 +30,12 @@ server_url=https://fishers.facman.site/
 profile_dir=${CHROME_PROFILE_DIR}
 chrome_path=${CHROME_BIN_PATH}
 EOF
-chown ${CHROME_USER} ${CONFIG_PATH}
 
-echo placards \& > "${CHROME_USER_HOME}/.xinitrc"
-chown ${CHROME_USER} "${CHROME_USER_HOME}/.xinitrc"
+echo placards \& > "~/.xinitrc"
 
 if [ ! -f setup.py ]; then
-    pip install https://github.com/247dink/placards-client-linux/@master#egg=placards_client
+    pip install git+https://github.com/247dink/placards-client-linux/@master#egg=placards
+
 else
     pip install .
 fi
