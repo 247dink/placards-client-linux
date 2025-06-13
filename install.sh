b6faef8378a1ed1ff87105b3662135aa141f4456
Ben Timby <btimby@gmail.com>
2025-06-13 13:44:08 -0400

    Try again.

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ if ! which pip; then
     sudo apt install -y python3-pip
 fi
 
-cat << EOF > "${CONFIG_PATH}"
+cat > "${CONFIG_PATH}" << EOF
 [placards]
 server_url=https://fishers.facman.site/
 profile_dir=${CHROME_PROFILE_DIR}
